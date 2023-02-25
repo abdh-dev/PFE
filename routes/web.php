212da78160f69b236651109b1bd44a1f46adb5ba
BruhR <44587812+BrahR@users.noreply.github.com>
2023-02-25 04:37:21 -0800

    Added projects route

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
-use App\Http\Controllers\ProfileController;
+    use App\Http\Controllers\Admin\ProjectController;
+    use App\Http\Controllers\ProfileController;
 use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
@@ -34,5 +35,9 @@ Route::middleware('auth')->group(function () {
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
+
+Route::resource('/projects', ProjectController::class)
+    ->only(['index', 'store', 'show', 'update', 'destroy']);
+//    ->middleware('auth');
 
 require __DIR__.'/auth.php';
