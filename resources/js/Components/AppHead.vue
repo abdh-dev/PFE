d3c971bd74d779907619ae94a02eec8e80a9d59f
BruhR <44587812+BrahR@users.noreply.github.com>
2023-02-25 05:38:21 -0800

    Added AppHead.vue to stop false errors

---
+++ resources/js/Components/AppHead.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+import { Head } from '@inertiajs/vue3'
+
+defineProps<{
+    title: string
+}>()
+</script>
+
+<template>
+    <Head>
+        <title>{{title}}</title>
+        <slot />
+    </Head>
+</template>
