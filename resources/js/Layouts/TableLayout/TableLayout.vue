6a71567814dd16f9920359c7fc06bd8fde4c3a9f
BruhR <44587812+BrahR@users.noreply.github.com>
2023-02-25 11:28:15 -0800

    Added a working table layout pog?

---
+++ resources/js/Layouts/TableLayout/TableLayout.vue
@@ -0,0 +1,63 @@
+<script setup lang="ts">
+import THead from '@/Layouts/TableLayout/THead.vue';
+import TableHead from '@/Layouts/TableLayout/TableHead.vue';
+import TableRow from '@/Layouts/TableLayout/TableRow.vue';
+import TData from '@/Layouts/TableLayout/TData.vue';
+
+import { computed } from 'vue'
+
+type Props = {
+    obj: Project[],
+    action: string[] | null,
+    max?: number
+}
+
+const props = withDefaults(defineProps<Props>(), {
+    action: null,
+    max: 4
+})
+
+const emit = defineEmits<{
+    (event: 'view', ...args: any[]): void
+    (event: 'delete', ...args: any[]): void
+}>()
+
+const filteredKeys = computed(() => {
+    return Object.keys(props.obj[0]).map(key => {
+        return key.split('_').map(word => word.charAt(0).toUpperCase() + word.slice(1)).join(' ')
+    })
+})
+</script>
+
+<template>
+    <div class="relative overflow-x-auto shadow-md sm:rounded-lg">
+        <table class="w-full text-sm text-left text-gray-500 dark:text-gray-400">
+            <TableHead>
+                <template v-for="(item, index) in filteredKeys">
+                    <THead :key="item" v-if="index <= max">
+                        {{ item }}
+                    </THead>
+                </template>
+                <THead v-if="action !== null">
+                    Action
+                </THead>
+            </TableHead>
+            <tbody>
+            <TableRow v-for="(item, index) in obj" :key="index" :border="index + 1 !== obj.length" >
+                <template v-for="(value, key, indexData) in item">
+                    <TData :key="key" v-if="indexData <= max">
+                        {{ value }}
+                    </TData>
+                </template>
+                <TData v-if="action !== null">
+                    <template v-for="(actionItem, index) in action" :key="index">
+                        <a class="text-blue-500 hover:text-blue-600 mr-1" @click="$emit(actionItem.toLowerCase(), item)">
+                            {{ actionItem }}
+                        </a>
+                    </template>
+                </TData>
+            </TableRow>
+            </tbody>
+        </table>
+    </div>
+</template>
---
+++ resources/js/Layouts/TableLayout/TableRow.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+import { computed } from 'vue'
+
+const props = defineProps<{
+    border: boolean
+}>()
+
+const borderClass = computed(() => {
+    return props.border ? 'bg-white border-b dark:bg-gray-800 dark:border-gray-700' : 'bg-white dark:bg-gray-800'
+})
+
+</script>
+
+<template>
+    <tr :class="borderClass">
+        <slot />
+    </tr>
+</template>
