c2ddd1b479ffefffded704536eac9583193c0076
BruhR <44587812+BrahR@users.noreply.github.com>
2023-02-25 05:20:45 -0800

    Added types to TextInput.vue and disabled support

--- resources/js/Components/TextInput.vue
+++ resources/js/Components/TextInput.vue
@@ -1,26 +1,37 @@
-<script setup>
+<script setup lang="ts">
 import { onMounted, ref } from 'vue';
 
-defineProps(['modelValue']);
+type PropsType = {
+    modelValue: string | number | boolean | null | undefined | string[],
+    disabled?: boolean,
+};
 
-defineEmits(['update:modelValue']);
+const props = withDefaults(defineProps<PropsType>(), {
+    modelValue: '',
+    disabled: false,
+});
 
-const input = ref(null);
+const emit = defineEmits<{
+    (event: 'update:modelValue', value: string | number): void,
+}>();
+
+const input = ref<HTMLInputElement | null>(null);
 
 onMounted(() => {
-    if (input.value.hasAttribute('autofocus')) {
-        input.value.focus();
+    if (input.value?.hasAttribute('autofocus')) {
+        input.value?.focus();
     }
 });
 
-defineExpose({ focus: () => input.value.focus() });
+defineExpose({ focus: () => input.value?.focus() });
 </script>
 
 <template>
     <input
         class="border-gray-300 focus:border-indigo-500 focus:ring-indigo-500 rounded-md shadow-sm"
-        :value="modelValue"
+        :value="props.modelValue"
         @input="$emit('update:modelValue', $event.target.value)"
         ref="input"
+        :disabled="props.disabled !== undefined ? props.disabled : false"
     />
 </template>
