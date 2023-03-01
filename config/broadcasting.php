eec0015744884376ccc59bb305d777304ee2912d
BruhR <44587812+BrahR@users.noreply.github.com>
2023-03-01 13:13:09 -0800

    Other websockets configurations

--- config/broadcasting.php
+++ config/broadcasting.php
@@ -36,15 +36,15 @@ return [
             'secret' => env('PUSHER_APP_SECRET'),
             'app_id' => env('PUSHER_APP_ID'),
             'options' => [
-                'host' => env('PUSHER_HOST') ?: 'api-'.env('PUSHER_APP_CLUSTER', 'mt1').'.pusher.com',
-                'port' => env('PUSHER_PORT', 443),
-                'scheme' => env('PUSHER_SCHEME', 'https'),
+                'host' => '127.0.0.1',
+                'port' => 6001,
+                'scheme' => 'http',
                 'encrypted' => true,
-                'useTLS' => env('PUSHER_SCHEME', 'https') === 'https',
+                'cluster' => env('PUSHER_APP_CLUSTER'),
             ],
-            'client_options' => [
-                // Guzzle client options: https://docs.guzzlephp.org/en/stable/request-options.html
-            ],
+//            'client_options' => [
+//                // Guzzle client options: https://docs.guzzlephp.org/en/stable/request-options.html
+//            ],
         ],
 
         'ably' => [
