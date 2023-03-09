aa82638b84b88da98a9b39ce766f923fff0d42d5
BruhR <44587812+BrahR@users.noreply.github.com>
2023-03-09 14:54:47 -0800

    Added some icons

---
+++ resources/js/Components/Icons/Plus.vue
@@ -0,0 +1,7 @@
+<template>
+    <div class="icon">
+        <svg viewBox="0 0 14 14">
+            <path fill-rule="evenodd" d="M7 0a1 1 0 0 0-1 1v5H1a1 1 0 0 0 0 2h5v5a1 1 0 1 0 2 0V8h5a1 1 0 1 0 0-2H8V1a1 1 0 0 0-1-1z" clip-rule="evenodd"></path>
+        </svg>
+    </div>
+</template>
---
+++ resources/js/Components/Icons/ThreeVDots.vue
@@ -0,0 +1,7 @@
+<template>
+    <div class="icon">
+        <svg viewBox="0 0 16 4">
+            <path fill-rule="evenodd" d="M2 4a2 2 0 1 1 0-4 2 2 0 0 1 0 4zm6 0a2 2 0 1 1 0-4 2 2 0 0 1 0 4zm6 0a2 2 0 1 1 0-4 2 2 0 0 1 0 4z"></path>
+        </svg>
+    </div>
+</template>
