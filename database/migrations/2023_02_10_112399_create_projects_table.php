34a6327ed74ee4f2e59a9656a517c3a630bca2d4
BruhR <44587812+BrahR@users.noreply.github.com>
2023-03-09 14:56:30 -0800

    Added color to the project table

--- database/migrations/2023_02_10_112399_create_projects_table.php
+++ database/migrations/2023_02_10_112399_create_projects_table.php
@@ -17,6 +17,7 @@ return new class extends Migration
             $table->id();
             $table->string("name");
             $table->string("description");
+            $table->string("color")->nullable();
             $table->date("timeline");
             $table->integer("budget")->nullable();
             $table->integer("resource_allocation")->nullable();
