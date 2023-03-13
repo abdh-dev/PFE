7d81479fdb0a4fd897d65d0f3aa8da66f531eb35
BruhR <44587812+BrahR@users.noreply.github.com>
2023-03-13 06:32:46 -0700

    Updated ProjectRequest.php to have color

--- app/Http/Requests/ProjectRequest.php
+++ app/Http/Requests/ProjectRequest.php
@@ -24,11 +24,14 @@ class ProjectRequest extends FormRequest
     public function rules()
     {
         return [
-            "name" => "required|string|max:255|unique:projects,name",
+            "name" => "required|string|max:255",
+//            "name" => "required|string|max:255|unique:projects,name",
             "description" => "required|string|max:255",
-            "timeline" => "required|date|after:today",
+//            "timeline" => "required|date|after:today",
+            "timeline" => "required|date",
             "budget" => "required|integer",
             "resource_allocation" => "required|integer",
+            "color" => "nullable|string|max:255",
         ];
     }
 }
