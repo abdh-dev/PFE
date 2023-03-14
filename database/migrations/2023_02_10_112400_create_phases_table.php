31fa5806e2497bc80be1e7dacdb1ecd076a18040
BruhR <44587812+BrahR@users.noreply.github.com>
2023-03-14 12:15:03 -0700

    Made phase.color nullable

--- database/migrations/2023_02_10_112400_create_phases_table.php
+++ database/migrations/2023_02_10_112400_create_phases_table.php
@@ -17,7 +17,7 @@ return new class extends Migration
             $table->id();
             $table->string("name");
             $table->string("description");
-            $table->string("color");
+            $table->string("color")->nullable();
 
             $table->foreignId("project_id")->nullable()->constrained();
             $table->timestamps();
