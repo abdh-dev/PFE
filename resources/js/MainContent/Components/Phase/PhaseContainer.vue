fb3c243c98a683e4af0ae7b89d9212416ea334a1
BruhR <44587812+BrahR@users.noreply.github.com>
2023-03-16 13:52:49 -0700

    Added NewTask to PhaseContainer.vue

--- resources/js/MainContent/Components/Phase/PhaseContainer.vue
+++ resources/js/MainContent/Components/Phase/PhaseContainer.vue
@@ -1,5 +1,6 @@
 <script setup lang="ts">
 import SideArrow from "@/Components/Icons/SideArrow.vue";
+import NewTask from "@/MainContent/Components/Task/NewTask.vue";
 
 import { ref } from "vue";
 
@@ -10,6 +11,15 @@ const props = defineProps<{
 }>()
 
 const isOpened = ref(true)
+
+const newTaskBtn = ref<HTMLButtonElement | null>(null);
+const isNewTaskOpen = ref(false);
+
+const showNewTask = (e: PointerEvent) => {
+    if (e.target == newTaskBtn.value! && isNewTaskOpen.value) return;
+    isNewTaskOpen.value = !isNewTaskOpen.value;
+};
+
 
 </script>
 
@@ -31,9 +41,11 @@ const isOpened = ref(true)
             <div class="side-arrow-dropdown-header-phase-title">
                 Phase {{ (index + 1) + " - " +  phase.name }}
             </div>
+            <button class="new-task-group" ref="newTaskBtn" @click="showNewTask">+ New task</button>
         </div>
         <div class="side-arrow-dropdown-content">
             <slot />
+            <NewTask v-if="isNewTaskOpen" v-click-outside="showNewTask" @close="showNewTask"/>
         </div>
     </div>
 </template>
