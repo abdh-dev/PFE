40dcb28ce3125d856b0eb95da13a270da2ac46b5
BruhR <44587812+BrahR@users.noreply.github.com>
2023-03-18 11:23:16 -0700

    Another day of having fun with ts

--- resources/js/types/models/phase.d.ts
+++ resources/js/types/models/phase.d.ts
@@ -4,6 +4,7 @@ type Phase = {
     description: string;
     color: string;
     tasks?: Task[];
+    tasks_count?: number;
     project_id?: number;
     created_at?: string /* Date */ | null;
     updated_at?: string /* Date */ | null;
