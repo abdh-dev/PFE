f216b83e56fe1b20690a5dece70f01aefac4a1f1
BruhR <44587812+BrahR@users.noreply.github.com>
2023-04-02 08:31:33 -0700

    Nothing personal kid

--- resources/js/hooks/useWebsocket.ts
+++ resources/js/hooks/useWebsocket.ts
@@ -1,20 +1,24 @@
-import { onMounted, onUnmounted } from "vue";
+import { onMounted, onUnmounted } from 'vue'
 
-export function useWebsocket(channel: string, events: { [key: string]: (data: any) => void }, privateChannel = false) {
-    let subbedChannel = Echo.channel(channel);
-    if (privateChannel) {
-        subbedChannel = Echo.private(channel);
+export function useWebsocket(
+  channel: string,
+  events: { [key: string]: (e: SocketEvent) => void },
+  privateChannel = true
+) {
+  let subbedChannel = Echo.channel(channel)
+  if (privateChannel) {
+    subbedChannel = Echo.private(channel)
+  }
+
+  onMounted(() => {
+    for (const [event, callback] of Object.entries(events)) {
+      subbedChannel.listen(event, callback)
     }
+  })
 
-    onMounted(() => {
-        for (const [event, callback] of Object.entries(events)) {
-            subbedChannel.listen(event, callback);
-        }
-    })
-
-    onUnmounted(() => {
-        for (const [event, callback] of Object.entries(events)) {
-            subbedChannel.stopListening(event, callback);
-        }
-    })
+  onUnmounted(() => {
+    for (const [event, callback] of Object.entries(events)) {
+      subbedChannel.stopListening(event, callback)
+    }
+  })
 }
