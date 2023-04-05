48410a597ff260439a37c77219ed014282a3b074
BruhR <44587812+BrahR@users.noreply.github.com>
2023-04-04 23:20:17 -0700

    project store, will probably change it later to use  multiple stores

---
+++ resources/js/stores/project.ts
@@ -0,0 +1,40 @@
+import { acceptHMRUpdate, defineStore } from 'pinia'
+import { Ref, ref } from 'vue'
+
+export const useProjectStore = defineStore('project', () => {
+  const projects = ref([]) as Ref<Project[]>
+  const project = ref<Project | null>(null)
+
+  function loadALl(data: Project[]) {
+    projects.value = data
+  }
+
+  function fetchProject(id: number | null): Project | undefined {
+    if (id === null || projects.value.length <= 0) return {} as Project
+    return projects.value.find((project) => project.id === id)
+  }
+
+  function pushPhase(idProject: number, ...phase: Phase[]) {
+    if (idProject === null || projects.value.length <= 0) return
+    fetchProject(idProject)?.phases?.push(...phase)
+  }
+
+  function pushTask(idProject: number, idPhase: number, ...task: Task[]) {
+    if (idProject === null || projects.value.length <= 0) return
+    const phases = fetchProject(idProject)?.phases?.find(
+      (phase) => phase.id === idPhase
+    ) as Phase
+    if (!phases.tasks) phases.tasks = []
+    phases.tasks.push(...task)
+  }
+
+  function push(...project: Project[]) {
+    projects.value.push(...project)
+  }
+
+  return { projects, project, push, loadALl, fetchProject, pushPhase, pushTask }
+})
+
+if (import.meta.hot) {
+  import.meta.hot.accept(acceptHMRUpdate(useProjectStore, import.meta.hot))
+}
