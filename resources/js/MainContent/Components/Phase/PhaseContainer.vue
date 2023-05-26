21602875e2964114c585d7bfeace7d467b7d6cc4
BruhR <44587812+BrahR@users.noreply.github.com>
2023-05-26 08:37:02 -0700

    Added task details for clicked tasks

--- resources/js/MainContent/Components/Phase/PhaseContainer.vue
+++ resources/js/MainContent/Components/Phase/PhaseContainer.vue
@@ -3,8 +3,10 @@ import SideArrow from '@/Components/Icons/SideArrow.vue'
 import NewTask from '@/MainContent/Components/Task/NewTask.vue'
 import TaskList from '@/MainContent/Components/Task/TaskList.vue'
 import Tasks from '@/MainContent/Components/Task/Tasks.vue'
+import TaskDetails from '@/MainContent/Components/Task/TaskDetails.vue'
 
 import { computed, Ref, ref } from 'vue'
+import PrettyJson from '@/Components/PrettyJson.vue'
 
 const props = defineProps<{
   project: Project
@@ -15,6 +17,9 @@ const props = defineProps<{
 const isOpened = ref(true)
 const newTaskBtn = ref<HTMLButtonElement | null>(null) as Ref<HTMLButtonElement>
 const isNewTaskOpen = ref(false)
+const detailsShown = ref(false)
+const selectedTask = ref<Task | null>(null)
+const selectedPhase = ref<Phase | null>(null)
 
 const showNewTask = (e: PointerEvent) => {
   if (e?.target == newTaskBtn.value && isNewTaskOpen.value) return
@@ -43,9 +48,27 @@ const groupedTasks = computed(() => {
   // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
   return groupSubTaskByParent(props.phase.tasks!)
 })
+
+const toggleTaskDetails = (e: PointerEvent, task: Task, phase: Phase) => {
+  // console.log(e?.currentTarget, e?.target)
+  console.log(e)
+
+  if (detailsShown.value) {
+    detailsShown.value = false
+    selectedPhase.value = null
+    selectedTask.value = null
+    return
+  }
+
+  selectedPhase.value = phase
+  selectedTask.value = task
+  detailsShown.value = true
+}
 </script>
 
 <template>
+  <!--  <PrettyJson :json="groupTaskByStatus(groupedTasks)" />-->
+  <!--  <PrettyJson :json="props.phase.tasks" />-->
   <div
     class="phase-container flex-grow-1"
     :style="{
@@ -89,8 +112,17 @@ const groupedTasks = computed(() => {
           :key="task.id"
           :task="task"
           :phase="phase"
+          @toggleTaskDetails="toggleTaskDetails"
         />
+        <!-- @click="(e: PointerEvent) => toggleTaskDetails(e, task, phase)" -->
       </TaskList>
     </div>
   </div>
+  <TaskDetails
+    v-if="detailsShown"
+    :task="selectedTask"
+    :phase="selectedPhase"
+    :project="project"
+    @toggle="toggleTaskDetails"
+  />
 </template>
